334edf6714f64f8d04f5952b9072f5c96b04f179
carol-ramos <carol.lmbg25@gmail.com>
2024-11-05 19:42:56 -0300

    adição de comentarios no codigo

--- app.py
+++ app.py
@@ -24,8 +24,15 @@ df = pd.DataFrame({
 @app.route('/table')
 def table():
     # Converte o DataFrame em uma tabela HTML
+    # O parâmetro 'classes' define a classe CSS para aplicar à tabela
+    # O parâmetro 'header' define se o cabeçalho será mostrado (true)
+    # O parâmetro 'index' define se o índice do DataFrame será exibido (false)
     table_html = df.to_html(classes='data', header="true", index=False)
+    
+    # Renderiza a página 'table.html' e passa a tabela gerada como parâmetro
     return render_template('table.html', table=table_html)
 
+# Verifica se o script está sendo executado diretamente (não importado)
+# Caso esteja, inicia o servidor da aplicação Flask no modo de desenvolvimento (debug=True)
 if __name__ == '__main__':
     app.run(debug=True)
